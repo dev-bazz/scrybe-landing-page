de53668ea9a29f6f2f673cdc2d3cd9d44a7e7a7c
bazz <clement.f.bazz@gmail.com>
2022-11-18 23:07:43 +0100

    Feat: Implemented the About Us UI

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './landingpage.module.scss'
 import heroImage from './assets/Group 1000000899.webp'
 import OurClient from '../../components/our-client'
-
+import aboutUsimage from './assets/Group 1000000931.webp'
 
 function LandingPage() {
         return (
@@ -21,15 +21,29 @@ function LandingPage() {
                                                         <a href="#" className={styles.ctaDemo}>Request a Demo</a>
                                                         <a href='#' className={styles.ctaStarter}>Get Started</a>
                                                 </div>
-
                                         </div>
                                         <div className={styles.heroImage}>
                                                 <img src={heroImage} alt="" srcset="" />
                                         </div>
                                 </div>
                         </section>
-                        <section>
+                        <section aria-label='Our Clients' >
                                 <OurClient />
+                        </section>
+
+                        <section >
+                                <div className={`container ${styles.aboutUs}`}>
+                                        <div className={styles.aboutUs__img}>
+                                                <img src={aboutUsimage} alt="" srcset="" />
+                                        </div>
+                                        <div className={styles.aboutUs__content}>
+                                                <h2 className={styles.aboutUs__title}>About Us</h2>
+                                                <p className={styles.aboutUs__details}>Scrybe is a conversation intelligence tool that automatically transcribes and analyzes recorded customer support conversations to extract
+                                                        sentimental analysis data. We help you determinethe efficiency of your customer support agents with the aim of improving overall customer satisfaction</p>
+                                                <a href="#" className={styles.aboutUs__cta}>Read more</a>
+
+                                        </div>
+                                </div>
                         </section>
 
                 </div>
