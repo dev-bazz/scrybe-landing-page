310650544eb008c8874839cbceb4c6cfb8358507
bazz <clement.f.bazz@gmail.com>
2022-11-18 19:49:02 +0100

    Chore: Styling Of Hero Section

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import styles from './landingpage.module.scss'
+import heroImage from './assets/Group 1000000899.webp'
+
 
 function LandingPage() {
         return (
                 <div >
-<section >
-        <div className={styles.container}>
-                <h1>Hello</h1>
-        </div>
+                        <section className={styles.hero} >
+                                <div className={styles.container}>
+                                        <div className={styles.content}>
+                                                <h1 className={styles.heroH1}>World class Transcription
+                                                        and <span className={styles.heroAccent}>
+                                                                Sentiment Analysis <wbr />
+                                                        </span>
+                                                        for yor Business</h1>
+                                                <p className={styles.heroText}>We help businesses unlock insights and extract meaningful
+                                                        data from your customer support conversations </p>
+                                                <div>
+                                                        <a href="#">CTA</a>
+                                                        <a href='#'> CTA</a>
+                                                </div>
 
-</section>
+                                        </div>
+                                        <div className={styles.heroImage}>
+                                                <img src={heroImage} alt="" srcset="" />
+                                        </div>
+                                </div>
+                        </section>
+                        <section>
+                                hello
+                        </section>
 
                 </div>
         )
