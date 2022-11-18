a273ea302aa83b94f1c07c4b9f772d1314074427
bazz <clement.f.bazz@gmail.com>
2022-11-18 18:21:28 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import LandingPage from './pages/landing-page'
+
+function App() {
+
+  return (
+    <LandingPage />
+
+  )
+}
+
+export default App
---
+++ src/pages/landing-page/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './landingpage.module.scss'
+
+function LandingPage() {
+        return (
+                <div >
+<section >
+        <div className={styles.container}>
+                <h1>Hello</h1>
+        </div>
+
+</section>
+
+                </div>
+        )
+}
+
+export default LandingPage
