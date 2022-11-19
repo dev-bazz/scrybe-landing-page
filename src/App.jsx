cb9f211444a6fbdde11ea49aff2cf1672bd993a0
bazz <clement.f.bazz@gmail.com>
2022-11-19 11:01:04 +0100

    Feat: Customer Testimonila Slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import LandingPage from './pages/landing-page-a/index'
 import {Routes, Route} from 'react-router-dom'
+import PageB from './pages/Landing-page-b'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<LandingPage />} />
+      <Route path='b' element={<PageB />} />
     </Routes>
     </>
 
---
+++ src/pages/Landing-page-b/index.jsx
@@ -0,0 +1,19 @@
+import CustomerSay from '../../components/customersSay'
+import Hero from '../../components/heroSection'
+import OurClient from '../../components/our-client'
+import styles from './pageB.module.scss'
+
+function PageB() {
+        return (
+                <>
+                        <Hero />
+                        <OurClient />
+                        <section className={styles.customer}>
+                        <h2 className={styles.customer__h2}>See what our clients  are saying</h2>
+                                <CustomerSay />
+                        </section>
+                </>
+        )
+}
+
+export default PageB
