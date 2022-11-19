b465bda2de93e779143eafab22f69bad19f31501
bazz <clement.f.bazz@gmail.com>
2022-11-19 08:57:42 +0100

    Fix: Landing Page Linking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.scss'
-import LandingPage from './pages/landing-page'
+import LandingPage from './pages/landing-page-a/index'
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
